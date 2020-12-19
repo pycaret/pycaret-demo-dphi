f60796a38ec59c5e2544587b4a46923465b449da
PyCaret <moez@pycaret.org>
2020-12-19 10:05:09 -0500

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,40 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=load_model('final-model')
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[x for x in request.form.values()]
+    final=np.array(int_features)
+    col = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+    data_unseen = pd.DataFrame([final], columns = col)
+    print(int_features)
+    print(final)
+    prediction=predict_model(model, data=data_unseen, round = 0)
+    prediction=int(prediction.Label[0])
+    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls throught request
+    '''
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 1234, debug = True)
